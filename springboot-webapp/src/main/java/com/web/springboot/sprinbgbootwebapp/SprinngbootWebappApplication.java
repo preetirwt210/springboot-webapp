aa0373a070ae39774b59252efbd5cc846d9f264a
Preeti Rawat <preetirawat711@gmail.com>
2021-02-22 17:44:56 +0530

    creating springboot -webapp

---
+++ springboot-webapp/src/main/java/com/web/springboot/sprinbgbootwebapp/SprinngbootWebappApplication.java
@@ -0,0 +1,13 @@
+package com.web.springboot.sprinbgbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinngbootWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinngbootWebappApplication.class, args);
+	}
+
+}
---
+++ springboot-webapp/src/test/java/com/web/springboot/sprinbgbootwebapp/SprinngbootWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.springboot.sprinbgbootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinngbootWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
